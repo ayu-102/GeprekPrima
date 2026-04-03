0f6ba0f1388fcec72e658552c1fe2b589cb78e68
ayu-102 <ayuningtiassubartha@gmail.com>
2026-04-03 21:31:03 +0700

    Perbaikan struk

--- resources/views/pesanan/struk.blade.php
+++ resources/views/pesanan/struk.blade.php
@@ -3,10 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Struk #{{ $order->id }} - Geprek Prima</title>
-
+    <title>Struk Kasir - Geprek Prima</title>
     <style>
         body {
             width: 100%;
@@ -14,14 +12,14 @@
             padding: 0;
             background: #fff;
             color: #000;
-            font-family: 'Courier New', Courier, monospace;
         }
 
         .struk {
-
-            width: 50mm;
+            width: 58mm;
             margin: 0 auto;
-            padding: 10px 5px;
+            padding: 10px 2px;
+            font-family: 'Courier New', Courier, monospace;
+            box-sizing: border-box;
         }
 
         .text-center {
@@ -52,30 +50,25 @@
         }
 
         .no-print {
-            margin-top: 20px;
+            margin-top: 15px;
             text-align: center;
         }
 
-
         @media print {
-            body {
-                background: none;
-            }
-
             .no-print {
                 display: none !important;
             }
 
             .struk {
-                width: 100%;
-                margin: 0;
-                padding: 0;
+                width: 58mm;
+                margin: 0 auto;
+                padding-left: 4mm;
+                padding-right: 4mm;
             }
 
             @page {
                 margin: 0;
                 size: 58mm auto;
-
             }
         }
     </style>
@@ -85,12 +78,12 @@
 
     <div class="struk">
         <div class="text-center">
-            <b style="font-size: 16px;">Geprek Prima</b><br>
-            <span style="font-size: 10px;">Canteen SMKN 4 Tangerang</span>
+            <b style="font-size: 14px;">GeprekPrima</b><br>
+            <span style="font-size: 10px;">Kasir</span>
 
             <div class="line"></div>
 
-            <div style="font-size: 11px; text-align: left;">
+            <div style="font-size: 10px; text-align: left;">
                 No : #{{ str_pad($order->id, 4, '0', STR_PAD_LEFT) }}<br>
                 Tgl : {{ $order->created_at->format('d/m/y H:i') }}<br>
                 Plgn : {{ strtoupper($order->nama_pembeli) }}
@@ -102,12 +95,12 @@
         <table>
             @foreach ($order->orderItems as $item)
                 <tr class="item-row">
-                    <td colspan="2" style="font-size: 12px;">{{ $item->menu->nama_menu }}</td>
+                    <td colspan="2" style="font-size: 11px;">{{ $item->menu->nama_menu }}</td>
                 </tr>
                 <tr class="item-row">
-                    <td style="font-size: 11px;">{{ $item->jumlah }} x
+                    <td style="font-size: 10px;">{{ $item->jumlah }} x
                         {{ number_format($item->menu->harga, 0, ',', '.') }}</td>
-                    <td class="text-right" style="font-size: 11px;">{{ number_format($item->subtotal, 0, ',', '.') }}
+                    <td class="text-right" style="font-size: 10px;">{{ number_format($item->subtotal, 0, ',', '.') }}
                     </td>
                 </tr>
             @endforeach
@@ -115,7 +108,7 @@
 
         <div class="line"></div>
 
-        <table style="font-size: 12px;">
+        <table style="font-size: 11px;">
             <tr class="bold">
                 <td>TOTAL</td>
                 <td class="text-right">Rp{{ number_format($order->total_harga, 0, ',', '.') }}</td>
@@ -139,28 +132,29 @@
 
         <div class="line"></div>
 
-        <div class="text-center" style="font-size: 11px;">
+        <div class="text-center" style="font-size: 10px;">
             <p style="margin: 0;">TERIMA KASIH</p>
             <p style="margin: 0;">Selamat Menikmati!</p>
         </div>
 
         <div class="no-print">
             <button onclick="window.print()"
-                style="font-size: 12px; padding: 10px; background: #FF6500; color: #fff; border: none; border-radius: 8px; cursor: pointer; width: 100%; font-weight: bold;">
-                <i class="bi bi-printer"></i> PRINT STRUK
+                style="font-size: 11px; padding: 8px 15px; background: #000; color: #fff; border: none; border-radius: 4px; cursor: pointer; width: 100%;">
+                PRINT SEKARANG
             </button>
-            <br><br>
-            <a href="{{ route('menu-user.index') }}"
-                style="text-decoration: none; color: #666; font-size: 11px;">Kembali ke Menu</a>
         </div>
     </div>
 
     <script>
         window.onload = function() {
             window.print();
-        }
+        };
 
-        window.onafterprint = function() {};
+
+        window.onafterprint = function() {
+
+            window.location.href = "{{ route('pesanan.struk') }}";
+        };
     </script>
 
 </body>
